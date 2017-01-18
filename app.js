d65472861fcd81c05ad9e941c013ee5454ee66c2
Monika Bharatsaran <M1037449@mindtree.com>
2017-01-18 16:30:48 +0530

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-
+app.use("/", express.static(__dirname + "/src"));
 app.get('/',function(request,response){
   response.sendFile(__dirname+'/index.html');
 });
@@ -8,5 +8,6 @@ app.get('/',function(request,response){
 var port = process.env.PORT || 8080;
 
 var server=app.listen(port,function(req,res){
+  console.log(__dirname);
     console.log("Catch the action at http://localhost:"+port);
 });
